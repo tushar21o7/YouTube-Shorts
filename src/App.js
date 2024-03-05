15315d2b2365e19b21fcba88766b3d0c62d1323c
Tushar Mishra <tusharmishra2107@gmail.com>
2024-03-05 19:21:41 +0530

    update app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-// import VideoContainer from "./pages/VideoContainer";
-// import Navbar from "./components/Navbar";
-
-import { useState, useEffect } from "react";
-import axios from "axios";
-import DisplayData from "./DisplayData";
+import VideoContainer from "./pages/VideoContainer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [data, setData] = useState(null);
-
-  const url = "https://mocki.io/v1/51bf5985-a727-40d2-9b80-3ae11e195047";
-
-  // fetch User details
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(url);
-      setData(response.data.list);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
-    <div>
-      <h1>Users</h1>
-      {data && <DisplayData data={data} />}
-    </div>
+    <>
+      <Navbar />
+      <VideoContainer />
+    </>
   );
 }
 
